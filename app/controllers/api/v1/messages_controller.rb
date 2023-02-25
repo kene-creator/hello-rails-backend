19dc31ae40b028c7be4de24c7e0b06e3c7679aca
Kene <kayyungx@gmail.com>
2023-02-25 18:16:28 +0100

    update message controller

--- app/controllers/api/v1/messages_controller.rb
+++ app/controllers/api/v1/messages_controller.rb
@@ -2,7 +2,7 @@ module Api
   module V1
     class MessagesController < ApplicationController
       def index
-        @greetings = Message.all
+        @greetings = Message.order('RANDOM()').first
         render json: @greetings
       end
     end
